a39fa7b87193eb25621285f4d2fc92d212ff9e43
Daniel Mendes Guedes <danieldf100@outlook.com>
2020-11-07 14:34:07 -0300

    Sum

---
+++ ProgramaTeste/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ProgramaTeste
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Training-console-app/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Training_console_app
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int nrdevezes = 10;
+
+            for (int i=0; i < nrdevezes; i++)
+            {
+                Console.WriteLine($"Olá Daniel! Você está avançando! .NET {i}");
+            }
+        }
+    }
+}
